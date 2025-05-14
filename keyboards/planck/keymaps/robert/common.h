263256e6a58d012f5013a0f340e88d0f314206d8
Robert Štojs <robert@robertstojs.com>
2025-05-14 13:17:56 +0200

    feat: add robert keymap to planck

---
+++ keyboards/planck/keymaps/robert/common.h
@@ -0,0 +1,45 @@
+#include QMK_KEYBOARD_H
+
+enum planck_layers {
+    _BASE,
+    _SYMBOL,
+    _FKEYS,
+    _ADJUST,
+    _NAV,
+};
+
+enum planck_keycodes {
+    QWERTY = SAFE_RANGE,
+    COLEMAK,
+    BACKLIT,
+    CTRL_ESC,
+    NAV_SCLN,
+    COPYADDR,
+    SYMBOL,
+    MD_CODE,
+    MEH_ENT,
+    LOCK,
+};
+
+#define FKEYS MO(_FKEYS)
+
+#define WORD_L LALT(KC_LEFT)
+#define WORD_R LALT(KC_RIGHT)
+
+#define LINE_L LGUI(KC_LBRC)
+#define LINE_R LGUI(KC_RBRC)
+
+#define DEL_WRD LALT(KC_BSPC)
+
+#define TAB_L LCTL(LSFT(KC_TAB))
+#define TAB_R LCTL(KC_TAB)
+
+#define CUT   LGUI(KC_X)
+#define COPY  LGUI(KC_C)
+#define PASTE LGUI(KC_V)
+
+#define EMOJI LCTL(LGUI(KC_SPC))
+
+#define CAP_LST LGUI(LSFT(KC_7))        // Capture last screen region
+#define CAP_IMG LGUI(LSFT(KC_6))        // Capture portion of screen
+#define CAP_MOV LGUI(LSFT(KC_5))        // Capture video of screen
---
+++ keyboards/planck/keymaps/robert/readme.md
@@ -0,0 +1,15 @@
+# Robert's Colemak-DH Planck Layout
+
+A modified Colemak-DH layout for the Planck keyboard, based on ppy's keymap but simplified to focus on the most useful features.
+
+## Features
+
+- Colemak-DH base layer
+- Symbol layer with all coding symbols
+- Function keys layer with mouse controls
+- Navigation layer accessed via semicolon
+- Adjust layer for keyboard settings
+- Special key combinations (CTRL_ESC, MEH_ENT)
+- Backtick surround functionality (press both shifts to surround with backticks)
+- Right shift tapped produces a dash (-)
+- Left+Right shift held together creates dash/underscore (_)
